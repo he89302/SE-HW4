6871c0ef7f7a9da13c08c3964da07f4dec01e7cd
wenpo huang <he89302@gmail.com>
2019-12-24 16:11:48 +0800

    problem 6

--- src/test/java/letterGradeTest.java
+++ src/test/java/letterGradeTest.java
@@ -5,6 +5,40 @@ import static org.junit.Assert.*;
 public class letterGradeTest {
 
     @Test
+    public void letterGradeInImpossibleByBoundariesTest() {
+        char gradeAInHeighestBoundary = LetterGrade.letterGrade(100);
+        assertEquals(gradeAInHeighestBoundary, 'A');
+        char gradeAInLowestBoundary =  LetterGrade.letterGrade(90);
+        assertEquals(gradeAInLowestBoundary, 'A');
+        
+        char gradeBInHeighestBoundary =  LetterGrade.letterGrade(89);
+        assertEquals(gradeBInHeighestBoundary, 'B');
+        char gradeBInLowestBoundary =  LetterGrade.letterGrade(80);
+        assertEquals(gradeBInLowestBoundary, 'B');
+        
+        char gradeCInHeighestBoundary =  LetterGrade.letterGrade(79);
+        assertEquals(gradeCInHeighestBoundary, 'C');
+        char gradeCInLowestBoundary =  LetterGrade.letterGrade(70);
+        assertEquals(gradeCInLowestBoundary, 'C');
+        
+        char gradeDInHeighestBoundary =  LetterGrade.letterGrade(69);
+        assertEquals(gradeDInHeighestBoundary, 'D');
+        char gradeDInLowestBoundary =  LetterGrade.letterGrade(60);
+        assertEquals(gradeDInLowestBoundary, 'D');
+        
+        char gradeFInHeighestBoundary =  LetterGrade.letterGrade(59);
+        assertEquals(gradeFInHeighestBoundary, 'F');
+        char gradeFInLowestBoundary =  LetterGrade.letterGrade(0);
+        assertEquals(gradeFInLowestBoundary, 'F');
+        
+        char gradeXInLowestBoundary = LetterGrade.letterGrade(-1);
+        assertEquals(gradeXInLowestBoundary, 88);
+        assertEquals(gradeXInLowestBoundary, 'X');
+        char gradeXInHeighestBoundary = LetterGrade.letterGrade(101);
+        assertEquals(gradeXInHeighestBoundary, 88);
+        
+    }
+    @Test
     public void letterGradeByPartitioningTest() {
         char gradeA =  LetterGrade.letterGrade(95);
         assertEquals(gradeA, 'A');
@@ -21,40 +55,5 @@ public class letterGradeTest {
         assertEquals(gradeLessZero, 'X');
         char gradeOverHundred = LetterGrade.letterGrade(101);
         assertEquals(gradeOverHundred, 88);
-    }
-
-    @Test
-    public void letterGradeInImpossibleByBoundariesTest() {
-        char gradeAInHeighestBoundary = LetterGrade.letterGrade(100);
-        assertEquals(gradeAInHeighestBoundary, 'A');
-        char gradeAInLowestBoundary =  LetterGrade.letterGrade(90);
-        assertEquals(gradeAInLowestBoundary, 'A');
-
-        char gradeBInHeighestBoundary =  LetterGrade.letterGrade(89);
-        assertEquals(gradeBInHeighestBoundary, 'B');
-        char gradeBInLowestBoundary =  LetterGrade.letterGrade(80);
-        assertEquals(gradeBInLowestBoundary, 'B');
-
-        char gradeCInHeighestBoundary =  LetterGrade.letterGrade(79);
-        assertEquals(gradeCInHeighestBoundary, 'C');
-        char gradeCInLowestBoundary =  LetterGrade.letterGrade(70);
-        assertEquals(gradeCInLowestBoundary, 'C');
-
-        char gradeDInHeighestBoundary =  LetterGrade.letterGrade(69);
-        assertEquals(gradeDInHeighestBoundary, 'D');
-        char gradeDInLowestBoundary =  LetterGrade.letterGrade(60);
-        assertEquals(gradeDInLowestBoundary, 'D');
-
-        char gradeFInHeighestBoundary =  LetterGrade.letterGrade(29);
-        assertEquals(gradeFInHeighestBoundary, 'F');
-        char gradeFInLowestBoundary =  LetterGrade.letterGrade(29);
-        assertEquals(gradeFInLowestBoundary, 'F');
-
-        char gradeXInLowestBoundary = LetterGrade.letterGrade(-1);
-        assertEquals(gradeXInLowestBoundary, 88);
-        assertEquals(gradeXInLowestBoundary, 'X');
-        char gradeXInHeighestBoundary = LetterGrade.letterGrade(101);
-        assertEquals(gradeXInHeighestBoundary, 88);
-        
     }
 }
