d12c57f32da249abc407eeec5c7a50227b2b6740
wenpo huang <he89302@gmail.com>
2019-12-24 16:26:49 +0800

    add main()

---
+++ src/main/java/LetterGradeMain.java
@@ -0,0 +1,20 @@
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class LetterGradeMain {
+    public static void main(String[] args) {
+        System.out.print("Enetr the score = ");
+        try {
+            InputStreamReader isr = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(isr);
+            Integer score = Integer.parseInt(br.readLine());
+            char grade = LetterGrade.letterGrade(score);
+            System.out.println("The grade of " + score + " is " + grade);
+        } catch (NumberFormatException ex) {
+            System.out.println("Not an integer!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
